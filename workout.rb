ac5d70ef8bfe2718b922dec6f82013ba388625f7
davepodgorski <davepodgorski@gmail.com>
2018-11-12 16:40:15 -0500

    energy_added

--- workout.rb
+++ workout.rb
@@ -1,16 +1,23 @@
 distance = 0
+energy = 100
 puts "Do you want to walk, run, or go home?"
 print "> "
 new_distance = gets.chomp
 until new_distance == "go home"
-  if  new_distance == "walk"
+  if energy <= 0 && new_distance == "run"
+    puts "You have no energy. You must walk."
+    puts "Do you want to walk or go home?"
+    new_distance = gets.chomp
+  elsif  new_distance == "walk"
      distance += 1
-puts "You are #{distance}km from home."
-puts "Do you want to walk, run, or go home?"
-new_distance = gets.chomp
+     energy += 1
+     puts "You are #{distance}km from home and your energy level is #{energy}."
+     puts "Do you want to walk, run, or go home?"
+     new_distance = gets.chomp
 elsif new_distance == "run"
   distance += 5
-  puts "You are #{distance}km from home."
+  energy -= 10
+  puts "You are #{distance}km from home and your energy level is #{energy}."
   puts "Do you want to walk, run, or go home?"
 new_distance = gets.chomp
 else
